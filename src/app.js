8f2f7a6aba510d764f31c9aec9ccab51a05297c4
Justin de Lisle <justin.delisle@rubik.com.au>
2015-04-23 19:37:34 +1000

    First commit

---
+++ src/app.js
@@ -0,0 +1,18 @@
+import { Component, View, bootstrap } from 'angular2/angular2';
+
+@Component({
+  selector: 'app'
+})
+
+@View({
+  templateUrl: 'app.html'
+})
+
+export class App {
+
+  constructor() {
+
+  }
+}
+
+bootstrap(App);
